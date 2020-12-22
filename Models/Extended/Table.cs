55e4afe0d798ca885db17a4af2c710f64afce1dc
Vasanthvivi <vivivijay57@gmail.com>
2020-12-22 21:59:11 +0530

    DataTables!

---
+++ Models/Extended/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Go.Models
+{
+    [MetadataType(typeof(TableMetaData))]
+    public partial class Table
+    {
+
+    }
+    public class TableMetaData
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please provide FirstName")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide LastName")]
+        public string LastName { get; set; }
+    }
+}
